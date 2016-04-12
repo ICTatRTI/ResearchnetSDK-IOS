9193f53aa72614499720747486e055aaceba5185
Adam Preston <adam.m.preston@gmail.com>
2016-04-12 09:27:42 -0600

    reimported with correct naming

---
+++ ResearchNet/ResearchNet/RFClass.h
@@ -0,0 +1,15 @@
+//
+//  RFClass.h
+//  ResearchNet
+//
+//  Created by Adam Preston on 4/12/16.
+//  Copyright © 2016 RTI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RFClass : NSObject
+
+-(void) printMessage:(NSString *) message;
+
+@end
