5d43348f0ae231b598e64b2f6b0c5ffbf027931b
Adam Preston <adam.m.preston@gmail.com>
2016-07-12 13:01:17 -0600

    implemented registration api

--- ResearchNet/Classes/ResearchNet.swift
+++ ResearchNet/Classes/ResearchNet.swift
@@ -6,16 +6,61 @@ public class ResearchNet: NSObject {
 
     var host: String
     var appKey: String
-    var object: RFClass = RFClass()
+    var userKey: String = ""
+
     
     public init(host: String, appKey: String) {
         self.host = host
         self.appKey = appKey
     }
     
+    
     public func printConfiguration() {
         NSLog("The host is ", host)
     }
+    
+    
+    public func enrollUser(completionHandler: (responseObject: String?, error: NSError?) -> (), username: String?, password: String?, first_name: String?, last_name: String?, gender: String?, dob: String?) {
+        
+        let parameters = [
+            "username": username!,
+            "password": password!,
+            "first_name": first_name!,
+            "last_name": last_name!,
+            "gender": gender!,
+            "email": username!,
+            "dob": dob!
+        ]
+        
+        // Enrolling users require the app key (because the user doesn't have a key of their own yet.
+        // Only use the appKey here!
+        
+        let headers = [
+            "Authorization": "Token \(appKey)"
+        ]
+        
+        Alamofire.request(.POST, "https://"+host+"/participant/", headers: headers, parameters: parameters)
+            .validate()
+
+            .responseJSON { response in switch response.result {
+                
+            case .Success(let data):
+                
+                let json = JSON(data)
+                completionHandler(responseObject: "" as String!, error: nil)
+                
+            case .Failure(let responseError):
+                
+                print("Request failed with error: \(responseError)")
+                completionHandler(responseObject: "hello" as String!, error: responseError)
+                
+                }
+        }
+        
+        
+    }
+    
+    
     
     
     public func authenticateUser(completionHandler: (responseObject: String?, error: NSError?) -> (), username: String?, password: String?) {
@@ -76,28 +121,17 @@ public class ResearchNet: NSObject {
         
         // call to submit api http://researchnet-documentation.s3-website-us-east-1.amazonaws.com/api/#survey-submission
         
-        
         let parameters = [
-            "username": username!,
-            "password": password!,
-            "first_name": first_name!,
-            "last_name": last_name!,
-            "gender": gender!,
-            "dob": dob!,
-            "email": username!
+            "response": username!,
+
         ]
         
         let headers = [
-            "Authorization": "Token \(appKey)"
+            "Authorization": "Token \(userKey)"
         ]
         
-        Alamofire.request(.POST, "https://"+host+"/participant/", headers: headers, parameters: parameters)
-            
-            /*
-             Alamofire treats any completed request to be successful, regardless of the content of the response. Calling validate before a response handler causes an error to be generated if the response had an unacceptable status code or MIME type. a swift 3 read more here: https://github.com/Alamofire/Alamofire#validation
-             */
+        Alamofire.request(.POST, "https://"+host+"/submission/", headers: headers, parameters: parameters)
             .validate()
-            //here you pass a new closure to the responseString method
             .responseJSON { response in switch response.result {
                 
             case .Success(let data):
